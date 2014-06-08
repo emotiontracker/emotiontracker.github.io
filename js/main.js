80a148a7160be140dbb20e66505af5bd5d2dfb16
Jugal Manjeshwar <jugalm9@gmail.com>
2014-06-08 17:11:06 -0400

    Update Calibration Mechanism

--- js/main.js
+++ js/main.js
@@ -19,6 +19,15 @@
         touchFeedback: true
     };
 
+    function createLine(x1, y1, x2, y2){
+        var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        line.setAttribute("x1", x1);
+        line.setAttribute("y1", y1);
+        line.setAttribute("x2", x2);
+        line.setAttribute("y2", y2);
+        return line;
+    }
+
     function getTouch(index){
         return touches[Object.keys(touches)[index]];
     }
@@ -30,56 +39,150 @@
     function calibrate(){
 
         var trials = [{
-            msg: '<span class="strong">Indicate Maximum Pleasure</span> <div class="light">Place any two fingers as far apart as comfortably possible.</div>',
+            title: 'Indicate Maximum Pleasure',
+            instr: 'Using two fingers, drag the circles as far apart as comfortably possible.',
             configVar: 'touchMaxDist'
         }, {
-            msg: '<span class="strong">Indicate Minimum Pleasure</span> <div class="light">Place any two fingers as close as comfortably possible.</div>',
+            title: 'Indicate Minimum Pleasure',
+            instr: 'Using two fingers, drag the circles as close as comfortably possible.',
             configVar: 'touchMinDist'
         }];
 
+
+
+        var paper = Raphael("tracker", windowWidth, windowHeight);
+
+        // var medianLine = paper.path("M0 " + windowHeight/2 + "L" + windowWidth + " " + windowHeight/2);
+        // medianLine.attr("stroke", "#eee");
+        // medianLine.attr("stroke-dasharray", "--");
+
+        var bubble = function(){
+            return {
+                x: 0,
+                y: windowHeight/2,
+                r: 40,
+                circle: null,
+                line: null,
+                selected: false,
+                update: function(){
+                    this.circle.attr("cx", this.x);
+                    this.circle.attr("cy", this.y);
+                    this.line.attr("path", "M" + windowWidth/2 + " " + windowHeight/2 + "L" + this.x + " " + this.y);
+                },
+
+                toggleSelected: function(){
+
+                    if(this.selected){
+                        this.circle.attr("fill-opacity", "0.2");
+                        this.circle.attr("r", "40");     
+                        this.selected = false;                      
+                    }
+                    else{
+                        this.circle.attr("fill-opacity", "0.6");
+                        this.circle.attr("r", "50");                       
+                    }
+                }
+            };
+        }
+
+        var bubbles = [new bubble(), new bubble()];
+        bubbles[0].x = windowWidth/2 - 30;
+        bubbles[1].x = windowWidth/2 + 30;
+
+        bubbles.forEach(function(b){
+            b.circle = paper.circle(b.x, b.y, b.r);
+            b.circle.attr("fill", "rgb(216, 0, 57)");
+            b.circle.attr("fill-opacity", "0.2");
+            b.circle.attr("stroke", "none");
+
+            b.line = paper.path("");
+            b.line.attr("stroke", "#000");
+            b.line.attr("stroke-opacity", "0.1");
+            b.update();
+        });
+
+        var joinLine = paper.path("M" + bubbles[0].x + " " + bubbles[0].y + "L" + bubbles[1].x + " " + bubbles[1].y);
+        joinLine.attr("stroke", "#c6003b");
+
+        var doubleDetect = false;
         var trialIndex = 0,
             trialCount = 1,
-            trialFail = [];
+            trialFail = [],
+            trial;
 
-        var messageEl = document.getElementById("messages");
+        function onTouchStart(e){
+            e.preventDefault();
+            var changed = e.changedTouches;
 
-        function startTrial(trialIndex){
+            for(var i = 0; i < changed.length ; i++){ 
 
-            var trial = trials[trialIndex];
+                bubbles.forEach(function(b){
+                    if(b.selected) return;
 
-            messageEl.innerHTML = trial.msg;
+                    if(b.circle.isPointInside(changed[i].pageX, changed[i].pageY)){
 
-            function onDoubleTouchEnd(){
+                        b.x = changed[i].pageX;
+                        b.y = changed[i].pageY;
+                        b.update();
+                        joinLine.attr("path", "M" + bubbles[0].x + " " + bubbles[0].y + "L" + bubbles[1].x + " " + bubbles[1].y);
+                        b.toggleSelected();
 
-                // var sampleX1 = 0, sampleY1 = 0,
-                //  sampleX2 = 0, sampleY2 = 0,
-                //  numSamples = 0;
+                        b.selected = changed[i].identifier;
+                    }
+                });
 
-                // var sampleInterval = setInterval(function(){
-                //  sampleX1 += getTouch(0).pageX;
-                //  sampleX2 += getTouch(1).pageX;
+            }
 
-                //  sampleY1 += getTouch(0).pageY;
-                //  sampleY2 += getTouch(1).pageY;
+            if(bubbles[0].selected && bubbles[1].selected){
+                doubleDetect = true;
+                instrEl.innerHTML = 'Release both fingers to set.';
+            }
+        }
 
-                // }, 500);
+        function onTouchMove(e){
+            e.preventDefault();
+            var changed = e.changedTouches; 
+            
+            for(var i = 0; i < changed.length ; i++){ 
 
-                // clearInterval(sampleInterval);
+                bubbles.forEach(function(b){
+                    if(b.selected && b.selected == changed[i].identifier){
+                        b.x = changed[i].pageX;
+                        b.y = changed[i].pageY;
+                        b.update();
+                        joinLine.attr("path", "M" + bubbles[0].x + " " + bubbles[0].y + "L" + bubbles[1].x + " " + bubbles[1].y);
+                    }
+                });
 
-                // var avgX1 = sampleX1 / numSamples,
-                //  avgX2 = sampleX2 / numSamples,
-                //  avgY1 = sampleY1 / numSamples,
-                //  avgY2 = sampleY2 / numSamples;
+            }            
+        }
 
-                window.removeEventListener('doubletouchend', onDoubleTouchEnd);
+        function onTouchEnd(e){
+            e.preventDefault();
+            var changed = e.changedTouches; 
+            
+            for(var i = 0; i < changed.length ; i++){ 
 
-                var sampleX1 = getTouch(0).pageX,
-                    sampleX2 = getTouch(1).pageX,
-                    sampleY1 = getTouch(0).pageY,
-                    sampleY2 = getTouch(1).pageY;
+                bubbles.forEach(function(b){
+                    if(b.selected === changed[i].identifier){
+                        b.toggleSelected();
+                    }
+                });
+
+            }    
+
+            if(!bubbles[0].selected && !bubbles[1].selected){
+
+                console.log("double end");
+                doubleDetect = false;
+                window.removeEventListener("touchstart", onTouchStart);
+
+                var sampleX1 = bubbles[0].x,
+                    sampleX2 = bubbles[1].x,
+                    sampleY1 = bubbles[0].y,
+                    sampleY2 = bubbles[1].y;
 
                 var dist = getDist(sampleX1, sampleY1, sampleX2, sampleY2);
-
                 if(trialCount == 2){
                     if( Math.abs(dist - config[trial.configVar]) > 100 ){
                         trialFail.push(trialIndex);
@@ -91,7 +194,7 @@
 
                 if(trialIndex == 0){
                     if(trialCount == 3){
-                        trialIndex = trialFail.pop();
+                        trialIndex = trialFail.splice(0,1)[0];
                     }
                     else{
                         trialIndex++;
@@ -103,30 +206,46 @@
                         trialIndex = 0;
                     }
                     else{
-                        trialIndex = trialFail.pop();
+                        trialIndex = trialFail.splice(0,1)[0];
                     }
                     trialCount++;
                 }
 
                 if(trialIndex !== undefined){
-                    messageEl.innerHTML = dist;
+                    instrEl.innerHTML = '';
                     setTimeout(function(){
                         startTrial(trialIndex);
-                    }, 2000);
+                    }, 1000);
                     
                 }
                 else{
-                    messageEl.innerHTML = 'Calibration Complete';
-                    config.touchFeedback = false;
-                }                
-            }
+                    document.getElementById("tracker").innerHTML = "";
+                    titleEl.innerHTML = 'Calibration Complete';
+                    instrEl.innerHTML = '';
+                }   
 
-            function onDoubleTouchStart(){
-                window.removeEventListener('doubletouchstart', onDoubleTouchStart);
-                window.addEventListener('doubletouchend', onDoubleTouchEnd);                
-            }
+            }       
+        }
 
-            window.addEventListener("doubletouchstart", onDoubleTouchStart);
+
+        window.addEventListener("touchmove", onTouchMove);
+        window.addEventListener("touchend", onTouchEnd);
+        window.addEventListener("touchcancel", onTouchEnd);
+
+
+        var titleEl = document.getElementById("calibTitle"),
+            instrEl = document.getElementById("calibInstr");
+
+        var startTrial = function(trialIndex){
+
+            window.addEventListener("touchstart", onTouchStart);
+
+            trial = trials[trialIndex];
+
+            console.log(trialIndex);
+
+            titleEl.innerHTML = trial.title;
+            instrEl.innerHTML = trial.instr;
         }
 
         startTrial(0);
@@ -158,11 +277,8 @@
             if(Object.keys(touches).length == 2){
 
                 if(config.touchFeedback){
-                    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-                    line.setAttribute("x1", getTouch(0).pageX);
-                    line.setAttribute("y1", getTouch(0).pageY);
-                    line.setAttribute("x2", getTouch(1).pageX);
-                    line.setAttribute("y2", getTouch(1).pageY);
+                    var line = createLine(  getTouch(0).pageX, getTouch(0).pageY,
+                                            getTouch(1).pageX, getTouch(1).pageY );
                     line.setAttribute("stroke", "#bbb");
 
                     touchLine = lineContainer.appendChild(line);
@@ -184,7 +300,7 @@
             }
         }
 
-        if(config.touchFeedback && Object.keys(touches).length == 2){
+        if(config.touchFeedback){
             touchLine.setAttribute("x1", getTouch(0).pageX);
             touchLine.setAttribute("y1", getTouch(0).pageY);
             touchLine.setAttribute("x2", getTouch(1).pageX);
@@ -221,11 +337,11 @@
         lineContainer.setAttribute("height", window.innerHeight);
     }
 
-    window.addEventListener("touchstart", handleTouchStart);
-    window.addEventListener("touchmove", handleTouchMove);
-    window.addEventListener("touchend", handleTouchEnd);
-    window.addEventListener("touchcancel", handleTouchEnd);
-    window.addEventListener("resize", handleResize);
+    // window.addEventListener("touchstart", handleTouchStart);
+    // window.addEventListener("touchmove", handleTouchMove);
+    // window.addEventListener("touchend", handleTouchEnd);
+    // window.addEventListener("touchcancel", handleTouchEnd);
+    // window.addEventListener("resize", handleResize);
 
     calibrate();
 })();
